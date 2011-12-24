d388f1b8e37e217f531ae6c5d56f36e038e5c8c2
Boko <bartheleway@live.fr>
2011-12-24 21:04:39 +0100

    Gestion des threads fini. Ajouts des classes manquantes. Petite réorganisation du code. Quelques morceaux de code commenté

--- src/Model/Fenetre.java
+++ src/Model/Fenetre.java
@@ -5,14 +5,11 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Observable;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-
-import Controller.Keyboard;
 
 import Threading.Print_aff;
 import Threading.Print_menu;
@@ -25,9 +22,9 @@ public class Fenetre extends JFrame implements Constantes {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private static Thread _aff = null;
-	private static Thread _calcPos = null;
-	private static Thread _menu = null;
+	private static Thread _threadAff = null;
+	private static Thread _threadCalcPos = null;
+	private static Thread _threadMenu = null;
 	public static Fenetre _fenster = null;
 	public static boolean _working = false;
 	
@@ -36,8 +33,6 @@ public class Fenetre extends JFrame implements Constantes {
 	private JMenuItem go = new JMenuItem("Lancer");
 	private JMenuItem pause = new JMenuItem("Pause");
 	private JMenuItem stop = new JMenuItem("Quitter");
-	private Thread game;
-	private Thread position;
 	private Bouton jouer = new Bouton("Jouer", 300, 100, 200, 50);
 	private Bouton charger = new Bouton("Charger partie", 300, 175, 200, 50);
 	private Bouton exit = new Bouton("Exit", 300, 250, 200, 50);
@@ -49,102 +44,94 @@ public class Fenetre extends JFrame implements Constantes {
 	static public Eggs oeufEnCours = null;
 	static public boolean _anime = false;
 	
-	private Affichage image;
-	private Menu menu = new Menu();
-	private MenuNiveau menuchargement = new MenuNiveau();
+	private Affichage _panelAff;
+	private Menu _panelMenu = new Menu();
+	private MenuNiveau _panelNiveau = new MenuNiveau();
 
 	public static void main(String[] args){
 		Fenetre._fenster = new Fenetre();
 		
 		if (Fenetre._fenster != null) {
 			Fenetre._working = true;
-			Fenetre._fenster.startThread();
+			Fenetre._fenster.startThreads();
 		}
 	}
 	
-	public void startThread () {
+	public void startThreads () {
 		while (_working) {
 			if (_anime) {
-				if (_aff == null && _calcPos == null) {
-					_aff = new Thread(new Print_aff());
-					_aff.start();
-					_calcPos = new Thread(new Calc_pos());
-					_calcPos.start();
+				if (_threadAff == null && _threadCalcPos == null) {
+					// Crée les threads d'affichage du niveau et de calcul des positions
+					_threadAff = new Thread(new Print_aff());
+					_threadCalcPos = new Thread(new Calc_pos());
 					
-					setContentPane(image);
-					setSize(800,600);
-
-					System.out.println("Cree le Thread affichage et calcul de position");
+					// Démarre les threads
+					_threadAff.start();
+					_threadCalcPos.start();
+					
+					// Affiche le panel du niveau dans la fenêtre et fixe la taille de la fenêtre
+					setContentPane(_panelAff);
 					setVisible(true);
+					setSize(800,600);
+					
+					System.out.println("Cree le Thread affichage et calcul de position");
 				}
 			} else {
-				if (_menu == null) {
-					_menu = new Thread(new Print_menu());
-					setContentPane(menu);
+				if (_threadMenu == null) {
+					// Crée le thread pour le menu
+					_threadMenu = new Thread(new Print_menu());
+					
+					// Démarre le thread
+					_threadMenu.start();
+					
+					// Affiche le panel du menu
+					setContentPane(_panelMenu);
+					setVisible(true);
 
 					System.out.println("Cree le Thread menu");
-					setVisible(true);
 				}
 			}
-
+			
+			// Vérifie l'état des threads toutes les demi-secondes
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
+		System.out.println("Quitte le jeu");
 		System.exit(0);
 	}
-
-	/*
-	class GoPlay implements Runnable {
-		public void run() {
-			printObj();
-		}
-	}
-	
-	class GoPos implements Runnable {
-		public void run() {
-			calc_pos();
-		}
-	}
-	*/
 	
 	public Fenetre() {
 		_working = true;
 		DataBase level = new DataBase();
 		level.loadLevel(1);
 		
-		image = new Affichage();
+		_panelAff = new Affichage();
 		
-		this.menu.add(jouer);
+		this._panelMenu.add(jouer);
 		jouer.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				_anime = true;
 				System.out.println("Bouton Jouer");
-				/*game = new Thread(new GoPlay());
-				game.start();
-				position = new Thread(new GoPos());
-				position.start();
-				*/
 			}
 		});
-		this.menu.add(exit);
+		this._panelMenu.add(exit);
 		exit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				//System.exit(0);
 				Fenetre._working = false;
 				System.out.println("Bouton Exit");
 			}
 		});
 		
-		this.menu.add(charger);
+		this._panelMenu.add(charger);
 		charger.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				Lister liste = new Lister();
 				File rep = new File(path);
-				liste.listerRepertoire(rep,menuchargement);
-				setContentPane(menuchargement);
+				liste.listerRepertoire(rep,_panelNiveau);
+				setContentPane(_panelNiveau);
 				setVisible(true);
 			}
 		});
@@ -153,8 +140,6 @@ public class Fenetre extends JFrame implements Constantes {
 		go.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				_anime = true;
-				/*game = new Thread(new GoPlay());
-				game.start();*/
 			}
 		});
 		
@@ -168,7 +153,7 @@ public class Fenetre extends JFrame implements Constantes {
 		this.menuedition.add(stop);
 		stop.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				System.exit(0);
+				_working = false;
 			}
 		});
 		
@@ -183,57 +168,10 @@ public class Fenetre extends JFrame implements Constantes {
 		this.setResizable(false);
 
 		this.setJMenuBar(menuBar);
-		//this.setContentPane(menu);
-
-		//this.setVisible(true);
 			
 	}
 	
-	public void printObj () {
-		//Suppression des oiseaux ayant collisionné
-		if(_list_birds.isEmpty() == false){
-			for(int i = 0; i < _list_birds.size(); i++) {
-				if (_list_birds.get(i).isDestructed()) {
-					_list_birds.remove(i);
-					//image.rm_elem_birds(i);
-				}
-			}
-		}
-		
-		this.repaint();
-	}
-	
-	public void calc_pos () {
-		for (int i = 0; i < _list_walkers.size(); i++) {
-			if (_list_walkers.get(i).collide_static() || this.outScreen(_list_walkers.get(i)))
-				_list_walkers.get(i).switchArriereState();
-			
-			_list_walkers.get(i).move(this);
-		}
-		
-					
-		//Deplacement des oiseaux
-		for(int i = 0; i < _list_birds.size(); i++){
-			if (_list_birds.get(i).collide_static() || _list_birds.get(i).collide_dynamic() || this.outScreen(_list_birds.get(i)) || _list_birds.get(i).isDestructing())
-				_list_birds.get(i).demol();
-			_list_birds.get(i).move();
-		}
-		
-		if(oeufEnCours != null) {
-			int c = oeufEnCours.collide();
-			if (c != -1){
-				_list_walkers.remove(c);
-				
-				oeufEnCours = null;
-			} else if (oeufEnCours.collide_static()){
-				oeufEnCours = null;
-			} else {
-				oeufEnCours.moveY(1);
-			}
-		}
-	}
-
-	private boolean outScreen(ItemDisplay item) {
+	public boolean outScreen(ItemDisplay item) {
 		if (item.getPosX() + item.getWidth() >= this.getWidth()) {
 			return true;
 		} else if (item.getPosX() <= 0) {
